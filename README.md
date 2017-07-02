bb4426f9a04cdf1ef3a69799c998d72c625f08c4
M3TIOR <jtimmerman32@gmail.com>
2017-07-02 03:09:41 -0700

    updated master branch to v0.09

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-#python3-noaa
+# python3-noaa
 
 This project is implemented to provide users with more elaborate access the NOAA's vast database,
-including managers for all of the different datasets comprised therein. This particular implement
-features both server query and response api's and temporary file storage method's to reduce overhead.
+including managers for all of the different datasets comprised therein.
+
+### Project Format:
+
+The sections of this API are divided as such,
+	1. primary database connections and workings are made available by the root package
+	2. each separate interface is connected to the noaa tree via a sub-module
+	3. the seperate interface sub-modules offer functions for interactions with direct data
+	4. and implement classes for custom data interaction if desired
+
+We're choosing this format because it's the most like the raw interface offered by the NOAA
+meaning we don't have to re-engineer the API, we just have to re-implement it for client side interaction.
 
 ### Database Assets:
- * NOAA's GHCN database,
+*Unmarked is incomplete, marked is finished.*
+ * [x] NOAA Database Connection (Project core)
+ * [] GHCN
+ * [] ***undecided***
+ * [] ***undecided***
+ * [] ***...***
+
+### Afterthoughts:
+
+We wanted to offer a custom database API. However, the NOAA is an international nonprofit dedicated to collecting real weather data and if there was a fraudulent NOAA clone somewhere hosting a bunch of madness for disproving global warming or whatever; we really don't want to take that chance. Sure, you could say we're lazy. But the reality is, we're just paranoid as hell after the 2016 election disaster...
--- src/noaa/__init__.py
+++ src/noaa/__init__.py
@@ -20,4 +20,79 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-version = "v0.0.1"
+#version = "v0.0.1"
+
+import tempfile
+import socket
+from ftplib import FTP
+
+class NOAADatabase():
+	"""
+		base class for NOAA database interactions.
+		Overrides basic connection information with correct
+		defaults, and ensure's we're managed properly
+	"""
+	root   = "/pub/data"   #Master Database directory
+	ftp    = None          #FTP connection
+
+	def __init__(self, email=''):
+		"""override init"""
+		self.ftp = FTP()
+		# Basically this everything the FTP init does,
+		# but I'm doing it here to remove the magical
+		# borkedness that is inheritence problems when calling
+		# inherited class functions (*-*) grrrrrrrrrrrr
+		self.connect()
+		# for the reccord, I was having problems with the timeout var
+		# in connect magically becoming the same value as the first argument
+		# of this init, despite my explicitly defining it as otherwise...
+		self.login(email)
+		# so yea, sorry for not being able to fix that, cause I'd include
+		# a variable timeout option, as extend the FTP class functionality,
+		# if it were possible anyway.
+
+	def __enter__(self):
+		"""override context"""
+		return self
+
+    # Context management protocol: try to quit() if active
+	def __exit__(self, *args):
+		"""override exit"""
+		self.ftp.__exit__(*args)
+
+	def connect(self, timeout=socket.SO_KEEPALIVE):
+		"""override connect"""
+		self.ftp.connect(host='ftp.ncdc.noaa.gov', 	# Always the same host
+				timeout=timeout)					# And keepalive timeout
+
+	def login(self, email=''):
+		"""override login"""
+		# FTP login defaults to anonymous without args,
+		# so we don't need to append any arguments here
+		# besides the password requested: which is the email
+		# of the user requesting access to the database
+		self.ftp.login(passwd=email)
+		# then move to the appropriate database directory
+		self.ftp.cwd('/pub/data')
+		# make absolutely sure passive mode is turned on!
+		self.ftp.set_pasv(True)
+
+	def set_blocksize(self, blocksize):
+		self._block = blocksize
+
+	def keep_alive():
+		self.ftp.voidcmd("NOOP")
+
+	#----------------------------------------------------------------#
+	# BEGIN RESTFUL ACTIONS
+	# I do have to admit, this is powerful, but not at all robust...
+	#----------------------------------------------------------------#
+	def send():
+		raise UnimplementedError()
+	def get():
+		raise UnimplementedError()
+	def relinquish(self, *files, dir=None):
+		raise UnimplementedError() # because lazy atm...
+	def request(self, dir=None, facts=None, callback=None, blocksize=8192):
+		for f in ftp.mlsd(dir, facts):
+			self.ftp.retrbinary("RETR %s"%())
